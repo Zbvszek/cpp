5d4717873c61be1cab3d2d530e05f23b27993e18
Grabowski <122259920+Zbvszek@users.noreply.github.com>
2023-11-21 14:55:04 +0100

    Add files via upload

--- zadanie_pola.cpp
+++ zadanie_pola.cpp
@@ -5,6 +5,10 @@ int trapez(int a,int b,int h) {
 	int pole = (a + b) * h / 2;
 	return pole;
 }
+struct figura{
+	char nazwa_figura[12];
+	int pole_figury;
+}
 
 
 int main(){
@@ -14,29 +18,34 @@ int main(){
 	//wszystkie zmienne dotyczace Pol zapisuj w struct
 	//do kazdego funckja
 	char choice;
-	int figura,a,b,h;
+	int figura_wybor,a,b,h,i=1;
 	while (1) {
 		printf("czy chcesz obliczyc pole figury? (t/n): ");
-		scanf("%c\n", &choice);
+		scanf_s("%c\n", &choice);
 		if (choice == 'n') {
 			break;
 		}
 		else {
 			printf("pole jakiej figury chcesz obliczyć:\n 1.Trapez\n 2.Kolo\n 3.trojkat\n 4.kwadrat\n 5.prostokat\n 6.rownoleglobok");
-			scanf("%d\n", &figura);
-			switch (figura) {
+			scanf_s("%d\n", &figura_wybor);
+			switch (figura_wybor) {
 			case 1:
 				printf("podaj bok a");
-				scanf("%d", &a);
+				scanf_s("%d", &a);
 
 				printf("podaj bok b");
-				scanf("%d", &b);
+				scanf_s("%d", &b);
 
 				printf("podaj bok h");
-				scanf("%d", &h);
+				scanf_s("%d", &h);
 
-				trapez(a, b, h);
+			
+				figura1.nazwa_figury = "Trapez";
+				figura1.polefigury = trapez(a, b, h);
+				break;
+				
 			}
+
 			
 		}
 
